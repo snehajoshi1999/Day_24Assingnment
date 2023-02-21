7bbc9cf63383bb525a99eacfd03f946202e2530e
snehajoshi1999 <120355215+snehajoshi1999@users.noreply.github.com>
2023-02-21 23:11:16 +0530

    Add files via upload
    Day 24 assinment

---
+++ Contact.java
@@ -0,0 +1,75 @@
+package com.brizlabs.streamapi;
+
+public class Contact {
+
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String email;
+    private String city;
+    private String zip;
+    private String state;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact [firstName=" + firstName + ", lastName=" + lastName + ", phoneNumber=" + phoneNumber
+                + ", email=" + email + ", city=" + city + ", zip=" + zip + ", state=" + state + "]";
+    }
+
+}
